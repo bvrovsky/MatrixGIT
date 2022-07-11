a2a69f84cec70ce128f1aba96f98a44880c70172
KowaretaSekai <egor.gusev1337@yandex.ru>
2022-07-11 22:02:05 +0300

    Added methods returnValue and putArr

--- src/MatrDemo.java
+++ src/MatrDemo.java
@@ -1,2 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MatrDemo {
+    public static void returnValue (float m) {
+        System.out.println(m);
+    }
+
+    public static void putArr(int[] X) //статический метод вывода вектора в терминал
+    {
+        for (int i = 0; i < X.length; i++)
+            System.out.printf("%d ", X[i]);
+        System.out.println();
+    }
 }
