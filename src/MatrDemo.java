26b082022d82026884a45d48e5eefcb5b530c8b0
KowaretaSekai <egor.gusev1337@yandex.ru>
2022-07-11 22:04:56 +0300

    Added method main

--- src/MatrDemo.java
+++ src/MatrDemo.java
@@ -21,4 +21,45 @@ public class MatrDemo {
             System.out.println();
         }
     }
+
+    public static void main(String[] args) throws IOException {
+        String fileName;//имя файла
+        String line = "";//приемник строки
+        String flag = "";//для выхода из цикла ввода с терминала
+        float[][] matr;//ссылка на матрицу-приемник
+        //определим поток для терминального ввода
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        do{
+            System.out.println("Enter the name of the matrix");
+            fileName = (br.readLine()).trim();//после ввода имени файла в
+            //окно терминала читаем его и обрезаем пробелы в начале и в конце
+            matr = IO.inpMatr(fileName);//ввод матрицы из файла
+            if (matr != null){
+                System.out.println("Matrix entered successfully");
+                //создаем объект класса Интеллектуальная матрица на базе введенной матрицы
+                IntellectMatr imatr = new IntellectMatr(matr);
+                //выводим исходную матрицу в окно терминала
+                System.out.println("Matrix introduced:");
+                imatr.putMatr();
+                //обрабатываем матрицу методами класса
+                //Интеллектуальная матрица и выводим промежуточные
+                //результаты обработки в окно терминала
+                System.out.println("Number of negative elements in matrix rows:");
+                putArr(imatr.negativeCntVector());
+                System.out.println("Sum of the elements of the main diagonal:");
+                System.out.println(imatr.sumDiagonal());
+                IntellectMatr imatr2 = new IntellectMatr(matr);
+                System.out.println("Determinant of matrix is:");
+                IntellectMatr imatr3 = new IntellectMatr(matr);
+                returnValue(imatr3.getValue());
+                System.out.println("Transposed matrix :");
+                IntellectMatr imatr4 = new IntellectMatr(matr);
+                putMatr(imatr4.TranspMatr());
+            }
+            System.out.println("Continue?: Yes - <Enter>, No - <n>");
+            flag = br.readLine().trim();
+        } while (!flag.equals("n"));//пока с терминала не введен символ n
+    }
+    //ввод с терминала буферизированный, т.е. происходит при нажатии Enter
 }
