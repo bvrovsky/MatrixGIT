58ed723361cd9e983bb7c11cd4a12cea1ef18b6e
bvrovsky <mr.volin.d@mail.ru>
2022-07-11 17:36:17 +0300

    Start

--- src/IntellectMatr.java
+++ src/IntellectMatr.java
@@ -1,2 +1,20 @@
 public class IntellectMatr {
+    //переменная экземпляра
+    private float[][] matr;//ссылка на прямоугольную матрицу
+    public float summ;
+    //конструктор
+    public IntellectMatr(float[][] matr){
+        this.matr = matr;
+    }
+    //геттер
+    public float[][] getMatr(){return matr;}
+    public void putMatr(){//вывод матрицы в окно терминала
+        if ((matr == null) || (matr[0] == null)) return;//матрица пуста
+        for (int i = 0; i < matr.length; i++){
+            for (int j = 0; j < matr[0].length; j++)
+                System.out.printf("% 7.2f", matr[i][j]);
+            System.out.println();
+        }//for i
+    }//putMatr
+    //вернуть сумму элементов главной диагонали матрицы
 }
